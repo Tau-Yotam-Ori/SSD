cb1a1bbd52e408811f1b848b065818fa2b394d92
Origithub <origlassman@mail.tau.ac.il>
2019-11-10 12:41:02 -0800

    random and sequential times are the same..

--- hw1/tests.c
+++ hw1/tests.c
@@ -2,7 +2,6 @@
 #include "common.h"
 #include <assert.h>
 
-#define MEMORY_USAGE 0.7
 #define RUN_TEST(SETUP, FUNC)   if(SETUP) popen("rm data/*.dat", "r"); \
 						        SSD_INIT(); \
  								FUNC; \
@@ -16,39 +15,39 @@ extern int32_t* mapping_table;
  */
 int test_access(int size, char type)
 {
-	int ret, i, lba, loop_size;
+	int ret, i, lba, request_size;
 
 	if (type == 's' || type == ' '){
-		if (type == ' '){
-			loop_size = SECTOR_NB;
+		if (type == 's'){
+			request_size = SECTORS_PER_PAGE * size; 
 		}
 		else {
-			loop_size = size*SECTORS_PER_PAGE;
+			request_size = SECTORS_PER_PAGE;
 		}
-		for(i = 0; i < loop_size; i += SECTORS_PER_PAGE){
+		for(i = 0; i*size <= SECTOR_NB*0.7; i += SECTORS_PER_PAGE){
 			if ((i/SECTORS_PER_PAGE) % 1024*10==0){
-				LOG("wrote %.3lf of device", (double)i  / (double)SECTOR_NB);
+				LOG("wrote %.3lf of device", (double)i*size / (double)SECTOR_NB);
 			}
-			printf("i: %d\n", i);
 
-			lba = i % (int)(MEMORY_USAGE*SECTOR_NB); // get valid lba
+			lba = (i*size % SECTOR_NB);
 
-			SSD_WRITE(SECTORS_PER_PAGE, lba);
+			SSD_WRITE(request_size, lba);
 		}
 
 		printf("wrote seq\n");
 	}
 	else {
-		for(i=0;i<size*SECTORS_PER_PAGE;i+=SECTORS_PER_PAGE){
+		request_size = SECTORS_PER_PAGE * size;
+		for(i = 0; i*size <= SECTOR_NB*0.7; i +=SECTORS_PER_PAGE){
 			if ((i/SECTORS_PER_PAGE) % 1024*10==0){
-				LOG("wrote %.3lf of device", (double)i  / (double)SECTOR_NB);
+				LOG("wrote %.3lf of device", (double)i*size  / (double)SECTOR_NB);
 			}
 			lba = rand() % (int)(0.7*SECTOR_NB); // get valid lba
 			
 
 			lba = lba - (lba % SECTORS_PER_PAGE); // align lba
 
-			SSD_WRITE(SECTORS_PER_PAGE, lba);
+			SSD_WRITE(request_size, lba);
 		}
 
 		printf("wrote random\n");
